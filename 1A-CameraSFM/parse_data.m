3462b9c434a14de752ac959f8705890185a0379b
Lorenzo Ciappetta <ciappetta.2011296@studenti.uniroma1.it>
2026-01-18 18:25:10 +0100

    improved dataset by replacing cell arrays with arrays for image keypoints

--- 1A-CameraSFM/parse_data.m
+++ 1A-CameraSFM/parse_data.m
@@ -22,7 +22,7 @@ function dataset = parse_data (filename)
             for i=1:3
                 camera.position(i) = str2num(l{i+2});
                 quat(i) =str2num(l{i+5});
-            end
+            endfor
 
             % compute rotation quaternion
 	        n = norm(quat)^2;
@@ -31,29 +31,39 @@ function dataset = parse_data (filename)
             else
 	            qw = sqrt(1 - n);
 	            camera.quaternion = [qw, quat(1), quat(2), quat(3)]; 
-            end
+            endif
 
             % initialize keypoints
-            camera.keypoints = cell();
+            camera.keypoints.ids = cell();
+            camera.keypoints.vectors = cell();
+            k = 1;
 
             dataset{id} = camera;
+
+            if id > 1
+                dataset{id-1}.keypoints.ids = cell2mat(dataset{id-1}.keypoints.ids);
+                dataset{id-1}.keypoints.vectors = reshape(cell2mat(dataset{id-1}.keypoints.vectors), 3, []).';
+            endif
         else
             % fill keypoints
             n = str2num(l{2})+1;
             m = str2num(l{3});
-            keypoint.id = m;
-            keypoint.v = zeros(1,3);
+            dataset{id}.keypoints.ids{k} = m;
+            dataset{id}.keypoints.vectors{k} = zeros(1,3);
+            
 
             for i=1:3
-                keypoint.v(i) = str2num(l{i+3});
-            end
-
-            dataset{id}.keypoints{n} = keypoint;  
+                dataset{id}.keypoints.vectors{k}(i) = str2num(l{i+3});
+            endfor
             
-        end
+            k+=1;
+        endif
 
         c = fgetl(fid);        
     end
     fclose(fid);
+
+    dataset{id}.keypoints.ids = cell2mat(dataset{id}.keypoints.ids);
+    dataset{id}.keypoints.vectors = reshape(cell2mat(dataset{id}.keypoints.vectors), 3, []).';    
     
 endfunction
